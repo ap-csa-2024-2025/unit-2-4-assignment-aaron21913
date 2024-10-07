5e6ea7085877829475114925f671d3e2d467de7c
aaron21913 <atngo106@student.hbuhsd.edu>
2024-10-07 11:00:59 -0700

    rizz balss

--- Main.java
+++ Main.java
@@ -2,11 +2,13 @@ public class Main
 {
   public static void main(String[] args)
   {
-    public static String lastFirstN(s1, s2, n)
+    public static String lastFirstN(s1, s2, int n)
     {
       string s1 = ();
       string s2 = ();
       int n = ();
+
+      0 <= n <= min(s1.length(), s2.length())
     }
   }
 }
